12a3f2a5d7736df388874cfe37554f9adbd3ce23
ben <smoilbig@126.com>
2018-09-03 00:14:12 +0800

    redisKeyService类的增加

---
+++ lib/core/RedisKeyService.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ben
+ * Date: 2018/9/2
+ * Time: 23:35
+ */
+
+namespace lib\core;
+
+
+class RedisKeyService
+{
+    public static function getRedisKey(...$args)
+    {
+        $len = count($args);
+        if ($len == 0) {
+            return "";
+        }
+
+        $keyTemplate = $args[0];
+        if ($len == 1)
+            return $keyTemplate;
+
+        preg_match_all("/{[A-Z_]+}/", $keyTemplate, $matches, PREG_PATTERN_ORDER);
+
+        if (count($matches[0]) == count($args) - 1) {
+            foreach ($matches[0] as $key => $match) {
+                $keyTemplate = str_replace($match, $args[$key + 1], $keyTemplate);
+            }
+        } else {
+            echo "args num is error";
+            return "";
+        }
+        return $keyTemplate;
+    }
+
+}
+
